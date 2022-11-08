c3a51aaec7ec819530d286c56931aa7a529c7755
Stanislav <gsv9026@mail.ru>
2022-11-08 17:48:50 +0300

    two task

--- task_js.js
+++ task_js.js
@@ -1,8 +1,18 @@
-//  Your classmates asked you to copy some paperwork for them. 
-//  You know that there are 'n' classmates and the paperwork has
-//  'm' pages.Your task is to calculate how many blank pages do 
-//  you need. If n < 0 or m < 0 return 0.
+//   Your classmates asked you to copy some paperwork for them. 
+//   You know that there are 'n' classmates and the paperwork has
+//   'm' pages.Your task is to calculate how many blank pages do 
+//   you need. If n < 0 or m < 0 return 0.
 
 function paperwork(n, m) {
     return n > 0 && m > 0 ? n * m : 0
-  }
+}
+
+
+//----------------------------------------------------------------------------------------------------
+
+//   Simple, remove the spaces from the string, then return the resultant
+//   string.
+
+function noSpace(x){
+    return x.split(' ').join('');
+ }
