68cc4d921549ab132c5344b2c35a7373200ff9dc
Stanislav <gsv9026@mail.ru>
2022-11-09 12:54:15 +0300

    two task

--- task_sql.sql
+++ task_sql.sql
@@ -4,3 +4,19 @@
 SELECT *
   FROM students
   WHERE IsActive
+
+---------------------------------------------------------------------------------------
+
+--You work at a book store. It's the end of the month, 
+--and you need to find out the 5 bestselling books at your store. 
+--Use a select statement to list names, authors, and number of copies 
+--sold of the 5 books which were sold most.
+
+
+SELECT name, author, copies_sold
+  FROM books
+  ORDER BY copies_sold DESC
+  LIMIT 5
+
+
+------------------------------------------------------------------------------------------------
