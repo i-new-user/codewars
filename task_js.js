429bdd059da04244c62d2cd262794d1c4fbd5f34
Stanislav <gsv9026@mail.ru>
2022-11-11 09:58:38 +0300

    eight tasc

--- task_js.js
+++ task_js.js
@@ -86,3 +86,16 @@ function doubleInteger(i) {
 
 
 
+
+//-----------------------------------------------------------------------------------------------------------
+
+//Complete the function to return his total number of goals in all three leagues.
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return +laLigaGoals + +copaDelReyGoals + +championsLeagueGoals;
+}
+
+
+
+
+
