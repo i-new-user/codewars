9e37c81c63ca7fc1177466df075f04681cec1302
Stanislav <gsv9026@mail.ru>
2022-11-11 09:56:16 +0300

    seven tasc

--- task_js.js
+++ task_js.js
@@ -74,3 +74,15 @@ function isDivisible(n, x, y) {
 
 
 
+
+
+//----------------------------------------------------------------------------------------------------------------
+
+//Code as fast as you can! You need to double the integer and return it.
+
+function doubleInteger(i) {
+    return i*2;
+}
+
+
+
