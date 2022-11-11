b8bbba18b0653e9413f9f7c228abeedd6079e673
Stanislav <gsv9026@mail.ru>
2022-11-11 12:51:35 +0300

    eleven tasc

--- task_js.js
+++ task_js.js
@@ -144,4 +144,30 @@ function greet(language) {
 
 function strCount(str, letter){  
     return str.split(letter).length-1
-  }
+}
+
+
+
+
+//-------------------------------------------------------------------------------------------------
+
+//Write a function that checks if a given string (case insensitive) is a palindrome.
+
+function isPalindrome(x) {
+    return x.toUpperCase() === x.split('').reverse().join('').toUpperCase();
+}
+
+
+
+
+
+//----------------------------------------------------------------------------------------------------
+
+//You can assume, for the purpose of this kata, that the supplied array will
+//not be empty.
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+        return Math.min(...args)
+    }
+}
