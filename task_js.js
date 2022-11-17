c2f47ed22af10758fb8933d37613d49b29a487d8
Stanislav <gsv9026@mail.ru>
2022-11-17 21:18:59 +0300

    Filter out the geese

--- task_js.js
+++ task_js.js
@@ -281,3 +281,9 @@ const square = function(x) {
 };
 console.log(square(12));
   
+
+//Filter out the geese
+function gooseFilter(birds){
+    let geese =  ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+    return birds.filter(item=> !geese.includes(item))
+}
