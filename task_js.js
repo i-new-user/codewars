0d2512985766d23d18bcd20ecd7c83dcd3819a17
Stanislav <gsv9026@mail.ru>
2022-11-22 09:49:21 +0300

    Basic variable assignment

--- task_js.js
+++ task_js.js
@@ -318,3 +318,9 @@ function rowSumOddNumbers(n) {
 //Hello, Name or World!
 function hello(name){
     if (name){ return "Hello, " + name.substring(0,1).toUpperCase() + name.substring(1).toLowerCase() + '!'; } else { return "Hello, World!"; } }
+
+
+//Basic variable assignment
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
